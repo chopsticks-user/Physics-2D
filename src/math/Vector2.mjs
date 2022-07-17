c0739b2537b9c79d1b47153d5be48acdc5e48ea5
Frost <huuquangcap@gmail.com>
2022-07-17 17:21:25 -0400

    Added vector operators

--- src/math/Vector2.mjs
+++ src/math/Vector2.mjs
@@ -7,29 +7,51 @@ import Neko2D from "../../Neko2D.mjs"
             this.x = x;
             this.y = y;
         }
+
         static fromPoints = (initialP, terminalP) => {
             return new module.V2(terminalP.x - initialP.x, terminalP.y - initialP.y);
         }
+
         static fromProperties = (magnitude, direction) => {
             const x = module.sqrt(magnitude ** 2 / (module.tan(direction) ** 2 + 1));
             const y = x * module.tan(direction);
             return new module.V2(x, y);
         }
+
+        static sum = (...vectors) => {
+            let [x, y] = [0, 0];
+            vectors.forEach((vector) => {
+                x += vector.x;
+                y += vector.y;
+            });
+            return new module.V2(x, y);
+        }
+
+        static dotProduct = (rVector, lVector) => {
+            return rVector.x * lVector.x + rVector.y * lVector.y;
+        }
+
+        static crossProduct = (rVector, lVector) => {
+            return rVector.x * lVector.y - rVector.y * lVector.x;
+        }
+
         magnitude = () => module.sqrt(this.x ** 2 + this.y ** 2);
+
         direction = () => {
             if (this.x === 0) {
                 return module.sign(this.y) * module.PI / 2;
             }
             return module.arctan(this.y / this.x);
         };
+
         unit = () => {
             const mag = this.magnitude();
             return new module.V2(this.x / mag, this.y / mag);
         }
+
         normal = () => {
             return new module.V2(- this.x, this.y);
         }
-        // getCoordinate = () => 
     }
     return module;
 })(Neko2D || {});
--- test.mjs
+++ test.mjs
@@ -4,4 +4,7 @@ import Neko2D from "./Neko2D.module.js"
 console.log(Neko2D);
 
 const v = Neko2D.V2.fromProperties(1, Neko2D.PI / 4);
-console.log(v);
+const u = new Neko2D.V2(1, -1);
+console.log(Neko2D.V2.sum(u, v, v));
+console.log(Neko2D.V2.dotProduct(u, v));
+console.log(Neko2D.V2.crossProduct(v, u));
